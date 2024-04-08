6d7065d0d6b4c52ee0acf55e8b7f55e8d5724477
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-08 15:10:01 +0530

    tooltip styling

--- app/(dashboard)/_components/Sidebar/list.tsx
+++ app/(dashboard)/_components/Sidebar/list.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useOrganizationList } from "@clerk/nextjs"
-
+import Item from "./item"
 const List = () => {
     
     const {userMemberships} = useOrganizationList({
@@ -13,9 +13,7 @@ const List = () => {
     return (  
         <ul className="space-y-4">
             {userMemberships.data?.map((mem) => (
-                <p key={mem.organization.id}>
-                    {mem.organization.name}
-                </p>
+                <Item key={mem.organization.id} id={mem.organization.id} name={mem.organization.name} imageUrl={mem.organization.imageUrl} />
             ))}
         </ul>
     );
