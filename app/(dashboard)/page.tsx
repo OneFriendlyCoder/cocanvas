c3a981f843985f92998d312cf6ed6fdc17697f92
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-08 20:24:43 +0530

    first time user organization creating

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,8 @@
+import EmptyOrg from "./_components/empty-orgs";
 const DashBoard = () => {
     return (  
-        <div>
-            DashBoard root page
+        <div className="flex-1 bg-red-500 h-[calc(100%-80px)] p-6">
+            <EmptyOrg/>
         </div>
     );
 }
